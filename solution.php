fd5fa70d5df9cb4c28f518ce9cbaa7bbf44d8c98
Mohammad <mhmdsleiman905@gmail.com>
2025-12-27 14:44:34 +0200

    write comments

--- solution.php
+++ solution.php
@@ -5,12 +5,18 @@
     $is_student = true; //boolean
     $prog_languages = [0 => "Javascript", 1 => "PHP", 2 => "Java"]; //array
     
+    //for printing: get values true/false instead of 0/1
     if ($is_student)
         $result = "true";
     else
         $result = "false";
 
+    //print statement with .
     echo "\"My name is ".$name.", I am ".$age." years old, my height is ".$height." 'cm', it is ".$result." that I am a student, and my favorite \\programming languages\\ are ".$prog_languages[0]." and ".$prog_languages[1]." and ".$prog_languages[2].".\"";
+    
+    //break row
     echo "<br>";
+
+    //another print statement with {}
     echo "\"My name is {$name}, I am {$age} years old, my height is {$height} 'cm', it is {$result} that I am a student, and my favorite \\programming languages\\ are {$prog_languages[0]} and {$prog_languages[1]} and {$prog_languages[2]}.\"";
 ?>
