486bcd4a6f35fa44ccca6617fee1e5aedff768f5
Mohammad <mhmdsleiman905@gmail.com>
2025-12-27 14:33:58 +0200

    make a simple conditional statement to get true or false values instead of 0 or 1

--- solution.php
+++ solution.php
@@ -4,5 +4,10 @@
     $height = 182.00;   //float
     $is_student = true; //boolean
     $prog_languages = [0 => "Javascript", 1 => "PHP", 2 => "Java"]; //array
+    
+    if ($is_student)
+        $result = "true";
+    else
+        $result = "false";
 
 ?>
