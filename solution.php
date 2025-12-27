1481ce9b98e027cd8c0a6fa68f3623138c4a9a37
Mohammad <mhmdsleiman905@gmail.com>
2025-12-27 14:29:47 +0200

    create variables and indicate their datatypes

---
+++ solution.php
@@ -0,0 +1,8 @@
+<?php
+    $name = "Mohammad Sleiman"; //string
+    $age = 20;  //integer
+    $height = 182.00;   //float
+    $is_student = true; //boolean
+    $prog_languages = [0 => "Javascript", 1 => "PHP", 2 => "Java"]; //array
+
+?>
